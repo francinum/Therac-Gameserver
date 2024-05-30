381b59bb91b9291b8fac94b0ad4d520569076a86
Kapu1178 <75460809+Kapu1178@users.noreply.github.com>
2024-05-30 18:02:11 -0400

    prettier

--- tgui/packages/tgui/interfaces/TemplateConsole.js
+++ tgui/packages/tgui/interfaces/TemplateConsole.js
@@ -1,14 +1,22 @@
 import { flow } from 'common/fp';
 import { filter, sortBy } from 'common/collections';
 import { useBackend, useSharedState } from '../backend';
-import { Button, Flex, Icon, Input, NoticeBox, Section, Stack, Table, Tabs } from '../components';
+import {
+  Button,
+  Flex,
+  Icon,
+  Input,
+  NoticeBox,
+  Section,
+  Stack,
+  Table,
+  Tabs,
+} from '../components';
 import { Window } from '../layouts';
 
 export const TemplateConsole = (props) => {
   return (
-    <Window
-      width={550}
-      height={500}>
+    <Window width={550} height={500}>
       <Window.Content scrollable>
         <RecipeContent />
       </Window.Content>
@@ -23,11 +31,7 @@ export const RecipeContent = (props) => {
       <Stack.Item>
         <Section title="Write To Disk">
           <RecipeLoader />
-          { data.disk_error && (
-            <NoticeBox>
-              {data.disk_error}
-            </NoticeBox>
-          )}
+          {data.disk_error && <NoticeBox>{data.disk_error}</NoticeBox>}
         </Section>
       </Stack.Item>
       <Stack.Item grow>
@@ -66,7 +70,8 @@ export const RecipeLoader = (props) => {
             onClick={() =>
               act('load', {
                 path: stagedRecipe.path,
-              })}
+              })
+            }
           >
             Load
           </Button>
@@ -77,9 +82,11 @@ export const RecipeLoader = (props) => {
 };
 
 const findRecipeByName = (name, recipe_index) => {
-  return Object.values(recipe_index)
-    .flatMap(category => category.recipes)
-    .find(recipe => recipe.name === name) || null;
+  return (
+    Object.values(recipe_index)
+      .flatMap((category) => category.recipes)
+      .find((recipe) => recipe.name === name) || null
+  );
 };
 
 /**
@@ -94,12 +101,14 @@ const searchForRecipes = (recipes, search) => {
   search = search.toLowerCase();
 
   return flow([
-    categories => categories.flatMap(category => category.recipes),
-    filter(recipe =>
-      recipe.name?.toLowerCase().includes(search.toLowerCase())
-      || recipe.desc?.toLowerCase().includes(search.toLowerCase())),
-    sortBy(recipe => recipe.name),
-    recipes => recipes.slice(0, 25),
+    (categories) => categories.flatMap((category) => category.recipes),
+    filter(
+      (recipe) =>
+        recipe.name?.toLowerCase().includes(search.toLowerCase()) ||
+        recipe.desc?.toLowerCase().includes(search.toLowerCase()),
+    ),
+    sortBy((recipe) => recipe.name),
+    (recipes) => recipes.slice(0, 25),
   ])(recipes);
 };
 
@@ -109,42 +118,39 @@ export const RecipeCatalog = (props) => {
 
   const recipe_index = Object.values(data.recipe_index);
 
-  const [
-    activeCategoryName,
-    setActiveCategoryName,
-  ] = useSharedState('category', recipe_index[0]?.name);
+  const [activeCategoryName, setActiveCategoryName] = useSharedState(
+    'category',
+    recipe_index[0]?.name,
+  );
 
-  const [
-    searchText,
-    setSearchText,
-  ] = useSharedState("search_text", "");
+  const [searchText, setSearchText] = useSharedState('search_text', '');
 
-  const activeCategory = activeCategoryName === "search_results"
-    ? { recipes: searchForRecipes(recipe_index, searchText) }
-    : recipe_index.find(category => category.name === activeCategoryName);
+  const activeCategory =
+    activeCategoryName === 'search_results'
+      ? { recipes: searchForRecipes(recipe_index, searchText) }
+      : recipe_index.find((category) => category.name === activeCategoryName);
 
-  const [
-    stagedRecipeName,
-    setStagedRecipeName,
-  ] = useSharedState('staged_recipe_name', null);
+  const [stagedRecipeName, setStagedRecipeName] = useSharedState(
+    'staged_recipe_name',
+    null,
+  );
 
   return (
-    <Section
-      fill
-      title="Recipes"
-    >
+    <Section fill title="Recipes">
       <Flex>
         <Flex.Item ml={-1} mr={1}>
           <Tabs vertical>
             <Tabs.Tab
               key="search_results"
-              selected={activeCategoryName === "search_results"}>
+              selected={activeCategoryName === 'search_results'}
+            >
               <Stack align="baseline">
                 <Stack.Item>
                   <Icon name="search" />
                 </Stack.Item>
                 <Stack.Item grow>
-                  <Input fluid
+                  <Input
+                    fluid
                     placeholder="Search..."
                     value={searchText}
                     onInput={(e, value) => {
@@ -154,8 +160,8 @@ export const RecipeCatalog = (props) => {
 
                       if (value.length) {
                         // Start showing results
-                        setActiveCategoryName("search_results");
-                      } else if (activeCategoryName === "search_results") {
+                        setActiveCategoryName('search_results');
+                      } else if (activeCategoryName === 'search_results') {
                         // return to normal category
                         setActiveCategoryName(recipe_index[0]?.name);
                       }
@@ -167,18 +173,20 @@ export const RecipeCatalog = (props) => {
                       if (onInput) {
                         onInput(e, value);
                       }
-                    }} />
+                    }}
+                  />
                 </Stack.Item>
               </Stack>
             </Tabs.Tab>
-            {recipe_index.map(category => (
+            {recipe_index.map((category) => (
               <Tabs.Tab
                 key={category.name}
                 selected={category.name === activeCategoryName}
                 onClick={() => {
                   setActiveCategoryName(category.name);
-                  setSearchText("");
-                }}>
+                  setSearchText('');
+                }}
+              >
                 {category.name} ({category.recipes.length})
               </Tabs.Tab>
             ))}
@@ -186,21 +194,17 @@ export const RecipeCatalog = (props) => {
         </Flex.Item>
         <Flex.Item grow={1} basis={0}>
           <Table>
-            {activeCategory?.recipes.map(recipe => {
+            {activeCategory?.recipes.map((recipe) => {
               return (
-                <Table.Row
-                  key={recipe.name}
-                  className="candystripe">
-                  <Table.Cell>
-                    {recipe.name}
-                  </Table.Cell>
-                  <Table.Cell
-                    collapsing
-                    textAlign="right">
+                <Table.Row key={recipe.name} className="candystripe">
+                  <Table.Cell>{recipe.name}</Table.Cell>
+                  <Table.Cell collapsing textAlign="right">
                     <Button
                       tooltip={recipe.desc}
                       tooltipPosition="left"
-                      onClick={() => { setStagedRecipeName(recipe.name); }}
+                      onClick={() => {
+                        setStagedRecipeName(recipe.name);
+                      }}
                     >
                       Select
                     </Button>
